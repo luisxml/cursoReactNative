6a41f3e6ef682e2632a74944fd7ea7ed57af0d16
Luis Galicia <luis.galicia.local@supervan.pe>
2021-06-19 23:23:13 +0100

    appcontador

---
+++ AwesomeTSProject/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const App = () => {
+  return (
+    <View>
+
+      <Text>
+        Hola Mundo... con react native
+      </Text>
+      
+    </View>
+  )
+}
